e7ec8f64309526a2c64d53b79c6ca498dd9bd829
Arnaud Degroote <arnaud.degroote@isae-supaero.fr>
2015-06-04 15:15:48 +0200

    Initial scene for jsbsim <-> morse interaction

---
+++ default.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env morseexec
+
+""" Basic MORSE simulation scene for <morse-jsbsim> environment
+
+Feel free to edit this template as you like!
+"""
+
+from morse.builder import *
+
+robot = QUAD2012()
+
+teleport = Teleport()
+robot.append(teleport)
+teleport.alter('ECEF')
+teleport.alter('NED')
+teleport.add_stream('hla', 'jsbsim.middlware.hla.read_aircraft_input.AircraftPoseInput')
+
+# set 'fastmode' to True to switch to wireframe mode
+env = Environment('sandbox', fastmode = False)
+env.set_camera_location([-18.0, -6.7, 10.8])
+env.set_camera_rotation([1.09, 0, -1.14])
+env.properties(longitude = 1.26, latitude = 43.26, altitude = 130)
+env.configure_stream_manager(
+        'hla',
+         fom = 'aricraft.fed', name = 'Morse', federation = 'morse_fdm',
+         sync_point = 'Init', time_sync = True)
+
---
+++ src/jsbsim/middleware/hla/read_aircraft_input.py
@@ -0,0 +1,32 @@
+import logging; logger = logging.getLogger("morse." + __name__)
+from morse.middleware.hla.message_buffer import MessageBufferReader
+from morse.middleware.hla.abstract_hla import AbstractHLAInput
+
+class AircraftPoseInput(AbstractHLAInput):
+    def initialize(self):
+        AbstractHLAInput.initialize(self)
+
+        aircraft_handle = self.amb.object_handle('aircraft')
+
+        self.handle_position = self.amb.attribute_handle("position", aircraft_handle)
+        self.handle_orientation = self.amb.attribute_handle("orientation", aircraft_handle)
+
+        self.suscribe_attributes(aircraft_handle, [self.handle_position, self.handle_orientation])
+
+    def default(self, ci = 'unused'):
+        attributes = self.get_attributes()
+
+        if attributes:
+            pos_attr = attributes[self.handle_position]
+            rot_attr = attributes[self.handle_orientation]
+
+            if not pos_attr or not rot_attr:
+                return 
+
+            self.data['x'] = MessageBufferReader(pos_attr).read_double()
+            self.data['y'] = MessageBufferReader(pos_attr).read_double()
+            self.data['z'] = MessageBufferReader(pos_attr).read_double()
+            self.data['yaw'] = MessageBufferReader(rot_attr).read_double()
+            self.data['pitch'] = MessageBufferReader(rot_attr).read_double()
+            self.data['roll'] = MessageBufferReader(rot_attr).read_double()
+
